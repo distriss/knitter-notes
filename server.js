e51616d75d1ac6aca7da673164a4e76dcc80bde5
Triss Marsh <marshtriss@gmail.com>
2023-01-26 18:51:01 +0000

    server js setup

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+
+const logger = require("morgan");
+const connectDB = require("./config/database");
+
+
+// use .env in config
+require("dotenv").config({ path: "./config/.env" });
+
+// Passport config
+require("./config/passport")(passport)
