3c120efdeb7d55f322a130c3d3f3559c4d5ce979
Triss Marsh <marshtriss@gmail.com>
2023-01-30 16:58:19 +0000

    setting config connections

--- server.js
+++ server.js
@@ -3,13 +3,24 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
 
 
-// use .env in config
+// Use .env file in config folder
 require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
-require("./config/passport")(passport)
+require("./config/passport")(passport);
+
+// Connect to Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
