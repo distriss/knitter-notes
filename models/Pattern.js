701094e78afbfbe4024f50131a28a47f4c57708f
Triss Marsh <marshtriss@gmail.com>
2023-03-06 16:16:22 +0000

    reorginize post model and pattern model

--- models/Pattern.js
+++ models/Pattern.js
@@ -1,39 +1,9 @@
 const mongoose = require("mongoose");
 
-const PatternSchema = new mongoose.Schema({
-    pattern: {
+const PatternSchema = new mongoose.Schema({   
+    patternSection: {
         type: String,
-        default: 'original'
-    },
-    craft: {
-        type: String,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    yarn: {
-        type: String,
-        required: true,
-    },
-    yarnWeight: {
-        type: String,
-        required: true,
-    },
-    gauge: {
-        type: String,
-    },
-    needleSize: {
-        type: String,
-        required: true,
-    },
-    yardage: {
-        type: String,
-    },
-    sizes: {
-        type: String,
-        default: 'one size'
+        required: true,        
     },
     post: { // which post the pattern belongs to
         type: mongoose.Schema.Types.ObjectId,
--- models/Post.js
+++ models/Post.js
@@ -24,6 +24,37 @@ const PostSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
     },
+    origin: {
+        type: String,
+        default: 'original'
+    },
+    craft: {
+        type: String,
+    },
+    category: {
+        type: String,
+    },
+    yarn: {
+        type: String,
+        required: true,
+    },
+    yarnWeight: {
+        type: String,
+    },
+    gauge: {
+        type: String,
+    },
+    needleSize: {
+        type: String,
+        required: true,
+    },
+    yardage: {
+        type: String,
+    },
+    sizes: {
+        type: String,
+        default: 'one size'
+    },
     createdAt: {
         type: Date,
         default: Date.now,
