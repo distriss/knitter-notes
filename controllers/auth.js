f713e6d049e951af238eb807ce39b74bee3f3030
Triss Marsh <marshtriss@gmail.com>
2023-03-13 17:14:00 +0000

    fix log out error, typo in post routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,17 +48,19 @@ exports.postLogin= (req, res, next) => {
     })(req, res, next);
 };
 
-exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    console.log("User has logged out.");
     req.session.destroy((err) => {
-      if (err)
-        console.log("Error : Failed to destroy the session during logout.", err);
+      if (err) console.log("Error : Failed to destroy the session during logout.", err);
       req.user = null;
       res.redirect("/");
     });
-  };
+  });
+};
   
   exports.getSignup = (req, res) => {
     if (req.user) {
